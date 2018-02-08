53919d76a04135d0ae99826a942fa111d73bd733
Malik Graham <malikgraham15@yahoo.com>
2018-02-07 20:29:57 -0600

    Updated weather class to retrieve daily weather data

--- src/main/java/moodprove/weather/WeatherData.java
+++ src/main/java/moodprove/weather/WeatherData.java
@@ -1,8 +1,33 @@
 package moodprove.weather;
 
-public class WeatherData {
+import org.json.JSONArray;
+import org.json.JSONObject;
 
-	public static void getWeatherData() {
-		
+import moodprove.http.MoodProveHttp;
+
+public class WeatherData {
+	
+	private final static String WEATHER_API_DOMAIN = "https://api.darksky.net/forecast/%s/%s,%s";
+	
+	private final Double longitude;
+	
+	private final Double latitude;
+	
+	public WeatherData(Double longitude, Double latitude) {
+		this.longitude = longitude;
+		this.latitude = latitude;
 	}
+
+	public JSONArray getDailyWeatherData() {
+		String weatherResponse = MoodProveHttp.executeGet(String.format(WEATHER_API_DOMAIN, WeatherAPIClientInfo.WEATHER_API_KEY, longitude, latitude));
+		JSONObject weatherData = new JSONObject(weatherResponse);
+		JSONArray daily = weatherData.getJSONArray("daily");
+		return daily;
+	}
+	
+	public JSONObject getTodaysWeatherData() {
+		JSONArray weatherDailyArray = getDailyWeatherData();
+		return weatherDailyArray.getJSONObject(0);
+	}
+
 }
